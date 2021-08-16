1ffc27cb207ccba697565840dae31cedf67b136e
asra <assem.rh@outlook.com>
2021-08-17 00:23:35 +0300

    up

---
+++ skinet/API/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetProducts()
+        {
+            return Content("this is my first api");
+        }
+        [HttpGet("{id}")]
+        public ActionResult<int> GetProduct( int id)
+        {
+
+            return 0;
+        }
+
+        public void MyTestMethod()
+        {
+
+        }
+        
+
+
+    }
+}
